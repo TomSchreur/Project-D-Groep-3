3f9994a7f320e74febee13a8a71b8addbbb17b98
TomSchreur <74595520+TomSchreur@users.noreply.github.com>
2022-03-20 21:28:59 +0100

    Added temporary Product class + functionality to turn query results into Product objects

--- database_manual.py
+++ database_manual.py
@@ -13,30 +13,35 @@ def create_connection(db_file):
     return conn
 
 def createtable(cur):
-    cur.execute(""" CREATE TABLE IF NOT EXISTS Products (
-        id integer PRIMARY KEY AUTOINCREMENT, 
-        image text NOT NULL, 
-        name text NOT NULL, 
-        price real NOT NULL); """)#temporary string for image
+    with create_connection("database.db") as db: #way to run queries to the database
+        cur = db.cursor()
+        cur.execute(""" CREATE TABLE IF NOT EXISTS Products (
+            id integer PRIMARY KEY AUTOINCREMENT, 
+            image text NOT NULL, 
+            name text NOT NULL, 
+            price real NOT NULL); """)#temporary string for image
 
-def insertintotable(cur):
-    cur.execute(""" INSERT INTO Products (image,name,price) 
-        VALUES('image','image_name',20.5);""")#id is autoincrement, so doesn't need to be defined
+def insertintotable(cur, image, name, price):
+    with create_connection("database.db") as db:
+        cur = db.cursor()
+        cur.execute(""" INSERT INTO Products (image,name,price) 
+            VALUES(?,?,?);""",(image,name,price))#id is autoincrement, so doesn't need to be defined
 
-def selectfromtable(cur):
-    cur.execute("""SELECT * FROM Products;""")
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
+def selectfromtable():
+    with create_connection("database.db") as db:
+        cur = db.cursor()
+        cur.execute("""SELECT * FROM Products;""")
+        rows = cur.fetchall()
+        return rows
 
 
 if __name__ == '__main__':
-    with create_connection("database.db") as db: #way to run queries to the database
-        c = db.cursor()
-        #c.execute("""DROP TABLE Products;""") #uncomment to drop table
-        createtable(c)
-        insertintotable(c)
-        selectfromtable(c)
+    #with create_connection("database.db") as db: #uncomment to drop table
+    #   c = db.cursor()
+    #   c.execute("""DROP TABLE Products;""")
+    createtable()
+    insertintotable('image', 'image_name', 20.5)
+    selectfromtable()
 
 
 
