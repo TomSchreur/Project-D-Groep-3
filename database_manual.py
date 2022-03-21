dcb280b1e198688942cbff6875f0abdb06e41916
HuslenSB <0990204@hr.nl>
2022-03-21 16:19:10 +0100

    Some functions
    getImageNames()
    getImgData()
    insertImageTable()

--- database_manual.py
+++ database_manual.py
@@ -1,7 +1,10 @@
 import sqlite3
+import os
+import sys
 from sqlite3 import Error
 
 #weet niet zeker of sqlite via pip moet gebeuren (bij mij niet). zoja, toevoegen bij requirements.txt
+listImgNames=[]
 
 def create_connection(db_file):
     #create a database connection to a SQLite database, creates the file if it doesn't exist
@@ -35,13 +38,49 @@ def selectfromtable():
         return rows
 
 
+
+# Makes a list of images in dir
+def getImageNames():
+    for img in os.listdir("./static/img"):
+        listImgNames.append(img)
+    
+# Iterates through img list, gets img data, calls func to insert into table
+# Denk wel handig om later de img file te verwijderen. Na het inserten naar database
+# Verder deze func moet nog aangepast worden. Maak product object aan -> insert product to database
+
+def getImgData():
+    getImageNames()
+    os.chdir("./static/img")
+    for x in listImgNames:
+        if ".png" in x:
+            with open(x,"rb") as z:
+                data= z.read()
+                insertImageTable(imgName= x, img=data)
+                print("{} Added to Db".format(x))
+        elif ".jpg" in x:
+            with open(x, "rb") as z:
+                data = z.read()
+                insertImageTable(imgName= x, img=data)
+                print("{} Added to Db".format(x))
+
+# Inserts name & imgdata into table.
+def insertImageTable(imgName, img):
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+    cursor.execute("""CREATE TABLE IF NOT EXISTS testInsertImages(name TEXT, image BLOP)""")
+    cursor.execute("""INSERT INTO testInsertImages(name, image) VALUES(?,?)""",(imgName, img))
+    conn.commit()
+    cursor.close()
+    conn.close()
+
 if __name__ == '__main__':
     #with create_connection("database.db") as db: #uncomment to drop table
     #   c = db.cursor()
     #   c.execute("""DROP TABLE Products;""")
-    createtable()
-    insertintotable('image', 'image_name', 20.5)
-    selectfromtable()
+    #createtable()
+    #insertintotable('image', 'image_name', 20.5)
+    #selectfromtable()
+    getImgData()
 
 
 
