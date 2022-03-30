7b789ba0e84d26e2c387e8a81a096b554ad8c631
TomSchreur <74595520+TomSchreur@users.noreply.github.com>
2022-03-31 00:11:52 +0200

    Removed unnecessary code

--- Product_class.py
+++ Product_class.py
@@ -1,18 +1,8 @@
-import database_manual
-
 class Product:
-    def __init__(self, id, image, name, price): #creates a Product object with given values
+    def __init__(self, id, name, image_path, df_path, price, category): #creates a Product object with given values
         self.id = id
-        self.image = image
         self.name = name
+        self.image_path = image_path
+        self.df_path = df_path
         self.price = price
-    
-    def print_info(self): #prints all values of Product (mainly for testing)
-        print(f"{self.id} {self.image} {self.name} {self.price}")
-
-def db_to_Products(): #turns the result of a select query to the db into Product objects
-    rows = database_manual.selectfromtable()
-    for row in rows:
-        Product(*row).print_info()
-
-db_to_Products()
+        self.category = category
