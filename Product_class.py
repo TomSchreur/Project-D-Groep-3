e1bb30a8d3fcb5908e68bf0e1dd1c7be91280615
TomSchreur <74595520+TomSchreur@users.noreply.github.com>
2022-03-31 01:05:19 +0200

    Added method for visualisation of prices (50,- instead of 50.0)

--- Product_class.py
+++ Product_class.py
@@ -6,3 +6,23 @@ class Product:
         self.df_path = df_path
         self.price = price
         self.category = category
+    
+    def getPrice(self):
+        pstr = str(self.price)
+        cents = False
+        eurostr = ""
+        centstr = ""
+        for i in range(len(pstr)):
+            if cents:
+                centstr += pstr[i]
+            elif pstr[i] == ".":
+                eurostr += ","
+                cents = True
+            else:
+                eurostr += pstr[i]
+        if len(centstr) != 2:
+            if centstr == "0":
+                centstr = "-"
+            else:
+                centstr += "0"
+        return eurostr + centstr
