39051e8db1d1f91f7d8d592a8b9f0fd31aa08dd9
TomSchreur <74595520+TomSchreur@users.noreply.github.com>
2022-04-29 00:50:04 +0200

    Fixed paths for High Contrast (now shows images and has tts)

--- server.py
+++ server.py
@@ -28,7 +28,6 @@ Products = selectallFromTable("Products")
 
 @app.route('/', methods=['GET', 'POST',])
 def index():
-    ttspressed=False
     if request.method == 'POST':
         # removes directory 'static/uploaded' & file contained inside
         # uploaded contains the last-uploaded image by user
@@ -147,13 +146,14 @@ def highContrastSwitch():
         img = Image.open(file.stream)  # PIL image
         uploaded_img_path = "static/uploaded/" + datetime.now().isoformat().replace(":", ".") + "_" + file.filename
         img.save(uploaded_img_path)
+        uploaded_img_path = "../" + uploaded_img_path
 
         # Run search
         query = fe.extract(img)
         dists = np.linalg.norm(features-query, axis=1)  # L2 distances to features
         ids = np.argsort(dists)[:30]  # Top 30 results
-        scores = [(dists[id], features.products[id].image_path) for id in ids]
-        session["scores"] = scores
+        # scores = [(dists[id], features.products[id].image_path) for id in ids]
+        # session["scores"] = scores
 
         for id in ids:
             if((Products[id].name)+".mp3" in "./static/mp3files"):
@@ -162,25 +162,7 @@ def highContrastSwitch():
                 createTempProductMp3(Products[id].description, Products[id].name)
 
         # establish scores to pass to HTML
-        scores = [(dists[id], Products[id].image_path, Products[id].name, getPrice(Products[id].price, Products[id].discount), Products[id].tts_path) for id in ids]
-
-        # get names, to search for products in db.
-        # temp= [(img_paths[id]) for id in ids]
-        # listpaths = []
-        # for i in temp:
-        #     listpaths.append(os.path.basename(i))
-        # queryProduct = selectProducts(listpaths)
-        # ProductPrices = [queryProduct[i][2] for i in range(30)]
-        
-        # pathMp3 =[]
-        # for i in listpaths:
-        #     pathMp3.append(Path('./static/mp3files/'+i.replace('.png','.mp3')))
-
-        # scores = [(dists[id], img_paths[id], os.path.basename(img_paths[id])) for id in ids]
-        # # Add prices and mp3paths, to tuple
-        # for i in range(30):
-        #     scores[i] = (scores[i] + (ProductPrices[i],)+(pathMp3[i],))
-
+        scores = [(dists[id], "." + Products[id].image_path, Products[id].name, getPrice(Products[id].price, Products[id].discount), "." + Products[id].tts_path) for id in ids]
         return render_template('highContrastIndex.html',
                                query_path=uploaded_img_path,
                                scores=scores)
