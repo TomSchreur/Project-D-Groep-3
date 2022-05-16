ee38f7c4d42f618dd3e9abc41127a2d727f28374
Pemseles <TJBouwsema@gmail.com>
2022-05-16 20:35:21 +0200

    database loading works almost
    only category_id assigning is annoying, shouldn't be based on if it has the category in it's name

--- database_manual.py
+++ database_manual.py
@@ -1,5 +1,6 @@
 import sqlite3
 import os
+import json
 from DbClasses import Product, Category, getPrice
 import random
 from sqlite3 import Error
@@ -25,6 +26,8 @@ def createProducttable():
             category_id integer NOT NULL,
             description text NOT NULL,
             discount real,
+            product_page text NOT NULL,
+            image_link text NOT NULL,
             FOREIGN KEY(category_id) REFERENCES Categories(id)
             ); """)
 
@@ -40,8 +43,8 @@ def createCategorytable():
 def insertintoProductstable(name, price, category_id, description, discount):
     with create_connection("database.db") as db:
         cur = db.cursor()
-        cur.execute(""" INSERT INTO Products (name, price, category_id, description, discount) 
-            VALUES(?,?,?,?,?,?,?);""",(name, price, category_id, description, discount)) #id is autoincrement, so doesn't need to be defined
+        cur.execute(""" INSERT INTO Products (name, price, category_id, description, discount, product_page, image_link) 
+            VALUES(?,?,?,?,?,?,?);""",(name, price, category_id, description, discount, product_page, image_link)) #id is autoincrement, so doesn't need to be defined
 
 def insertintoCategorytable(product_type, category):
     with create_connection("database.db") as db:
@@ -78,80 +81,75 @@ def selectProducts(names):
     return removeNestedList
 
 # Makes a list of images in dir
-def getImageNames(dir):
-    for img in os.listdir(dir):
-        listImgNames.append(img)
-    return listImgNames
+def getProductsJSON():
+    productList = []
+    a = open('static/DbData.json', "r")
+    jsonObj = json.loads(a.read())
+    for i in jsonObj:
+        for j in jsonObj[i]:
+            for k, productInfo in jsonObj[i][j].items():
+                productList.append(productInfo)
+    a.close()
+    return productList
 
-# Image file naam, MOET gerelateerd zijn aan de product (bijv. een zwarte spijkerbroek -> filenaam: Dark Jeans (15))
+def getCategoriesJSON():
+    categoryList = []
+    a = open('static/DbData.json', "r")
+    jsonObj = json.loads(a.read())
+    for i in jsonObj:
+        for j in jsonObj[i]:
+            categoryList.append({'ProductType': i, 'Category': j})
+    a.close()
+    return categoryList
+
+# Alle relevante informatie staat in de DbData.json file
 # En dan bij de cursors.execute even de juiste categorie in parameters plaatsen.
-def insertProductTable(dir):
-    paths = getImageNames(dir)
+def insertProductTable():
+    productArr = getProductsJSON()
     conn = sqlite3.connect("database.db")
     cursor = conn.cursor()
-    for p in paths:
-        name = os.path.splitext(p)[0]
-        image_path = dir + "/" + p
-        price = float(random.randint(10,60))
+    for p in productArr:
+        name = p["Name"]
+        price = p["Price"]
         categories = selectallFromTable("Categories")
-        maxDiscount = 0.6
-        discount = round(random.random(), 2)
-        while discount > maxDiscount:
-            discount = round(random.random(), 2) # can be anything above 0.0; currently a random float (0.xx)
+        discount = p["Discount"]
+        product_page = p["ProductPage"]
+        image_link = p["ImageLink"]
         description = ""
+        category_id = 0
         for category in categories:
             if category.category.upper() in name.upper():
                 category_id = category.id
                 description = "The product name is: " + name + ". The price is € " + getPrice(price,discount) + ". The product type is: " + category.product_type + " of category " + category.category
                 break
-        # category = ""
-        # categories = ["Sweater & hoodies", "Sport Pants", "Sweater", "Jeans", "Shirt"]
-        # for cat in categories:
-        #     if cat.upper() in name.upper():
-        #         category = cat.upper()
-        #         continue
-        tts_path = "./static/mp3files/" + name + ".mp3"
-        cursor.execute("""INSERT OR IGNORE INTO Products (name, price, category_id, description, discount) VALUES(?,?,?,?,?,?,?)""", (name, image_path, price, category_id, tts_path, description, discount))
+        cursor.execute("""INSERT OR IGNORE INTO Products (name, price, category_id, description, discount, product_page, image_link) VALUES(?,?,?,?,?,?,?)""", (name, price, category_id, description, discount, product_page, image_link))
     conn.commit()
     cursor.close()
     conn.close()
 
-# "Sweater & hoodies", "sport pants", "Sweater", "Jeans", "Shirt"
-def insertCategoryTable(dir):
-    categories = ["T-Shirt", "Jeans", "Socks", "Vacuum Cleaners"] # expand later (make sure categories containing later categories like 'sweater & hoodies' and 'sweater' are earlier)
+def insertCategoryTable():
+    categories = getCategoriesJSON()
     conn = sqlite3.connect("database.db")
     cursor = conn.cursor()
     for c in categories:
-        cursor.execute("""INSERT OR IGNORE INTO Categories (product_type, category) VALUES(?,?)""", ("Clothing", c))
+        cursor.execute("""INSERT OR IGNORE INTO Categories (product_type, category) VALUES(?,?)""", (c["ProductType"], c["Category"]))
     conn.commit()
     cursor.close()
     conn.close()
 
 
-def createProducts():
-    Products = []
-    Products.append(Product(0, "Nike Park 20 Trainingsbroek Heren", "https://media.s-bol.com/NxZmYWvo6NMz/442x1200.jpg", 29.99, 2, "https://www.bol.com/nl/nl/p/nike-park-20-trainingsbroek-heren-maat-m/9200000128075467/?bltgh=tLuB0HrCrOKBld6oazhFjA.sQmD8xzkeTHgwPfAqRdBzQ_0_50.52.ProductTitle", None, None))      
-    return Products
-
-
 if __name__ == '__main__':
-    Products = createProducts()
-    for product in Products:
-        product.createDescription(selectallFromTable("Categories"))
-        print(product.product_page)
-    # with create_connection("database.db") as db: #uncomment to drop table
-    #     c = db.cursor()
-    #     c.execute("""DROP TABLE Products;""")
-    #     c.execute("""DROP TABLE Categories""")
-    # createProducttable()
-    # createCategorytable()
-    # insertCategoryTable('./static/img')
-    # insertProductTable('./static/img')
-    #print(selectallFromTable()[0].tts_path)
+    with create_connection("database.db") as db: #uncomment to drop table
+        c = db.cursor()
+        c.execute("""DROP TABLE Products;""")
+        c.execute("""DROP TABLE Categories""")
+    createProducttable()
+    createCategorytable()
+    insertCategoryTable()
+    insertProductTable()
 
 #categories = ["Sweater & Hoodies", "Sport Pants", "Sweater", "Jeans", "Shirt"] 
 #create list
 #for each product, add:
 #   Product(0, *Product Name (website)*, *image link*, *Product price*, *see categories for id*, *generate description*, *generate discount*)
 #foreach product in list, add to db
-
